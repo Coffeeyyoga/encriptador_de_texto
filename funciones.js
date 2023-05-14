fabb6ef2ccd0b18ccfd77dff52afb8414519c10f
Keren Parra <kerenandreina@gmail.com>
2023-05-14 17:11:55 -0500

    primer commit

---
+++ funciones.js
@@ -0,0 +1,60 @@
+let input = document.getElementById("input-text");
+let output = document.getElementById("output-text");
+
+function onlyContainsLowercase(str) {
+    return /^[a-z0-9/ /:;,.¿?¡!]+$/.test(str);
+}
+
+function encrypt() {
+    const chars = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"        
+    }
+
+    if (onlyContainsLowercase(input.value)) {
+        let encriptedText = input.value.replace(/[aeiou]/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = encriptedText;
+    } else {
+        alert("Recuerde, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("pegar").style.display = "none";
+}
+
+function desencrypt() {
+    const chars = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u"
+        }
+
+    if (onlyContainsLowercase(input.value)) {
+        let desencriptedText = input.value.replace(/ai|enter|imes|ober|ufat/g, c => chars[c]);
+        document.getElementById("not-found").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        output.textContent = desencriptedText;
+    } else {
+        alert("Recuerde, sólo letras minúsculas y sin acentos");
+    }
+
+    document.getElementById("pegar").style.display ="none";
+}
+
+function copy() {
+    let copyText = document.getElementById("output-text").innerText;
+    navigator.clipboard.writeText(copyText);
+    document.getElementById("pegar").style.display ="block";
+}
+
+function pegar() {
+    navigator.clipboard
+        .readText()
+        .then((clipText) => (input.value = clipText));
+}
